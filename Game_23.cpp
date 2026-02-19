f5ac3fb086239b3e1bf5acb59d0fce444d83e7f4
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-02-19 18:40:21 +0530

    Create Game_23.cpp

---
+++ Game_23.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    if(m%n != 0)
+    {
+        cout<<-1;
+        return 0;
+    }
+    int x = m/n,move = 0;
+    while(x%2 == 0)
+    {
+        x/= 2;
+        move++;
+    }
+    while(x%3 == 0)
+    {
+        x /= 3;
+        move++;
+    }
+    if(x!=1)
+    {
+        cout<<-1;
+    }else
+    {
+        cout<<move;
+    }
+    return 0;
+}
