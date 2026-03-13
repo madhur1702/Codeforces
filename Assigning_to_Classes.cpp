fff50bc94871f1cdc81993eea031b3eddbb10c2c
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-03-13 23:25:35 +0530

    Create Assigning_to_Classes.cpp

---
+++ Assigning_to_Classes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(n*2);
+        for(int i=0;i<n*2;i++)
+        {
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        cout<<a[n] - a[n-1]<<endl;
+    }
+    return 0;
+}
