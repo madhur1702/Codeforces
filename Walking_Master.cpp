7074d856e2653f48e332f91ce4f165d3a908e3b8
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-03-30 23:10:28 +0530

    Create Walking_Master.cpp

---
+++ Walking_Master.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(b>d)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        int moves = d - b;
+        a += moves;
+        if(a<c)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        moves += (a - c);
+        cout<<moves<<endl;
+    }
+    return 0;
+}
