a9f66211a6de406be0b9c46451d5845396e91cef
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-03-31 13:20:23 +0530

    Create We_Need_the_Zero.cpp

---
+++ We_Need_the_Zero.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        vector<long long> a(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            
+        }
+        long long total = 0;
+        for(int i=0;i<n;i++)
+        {
+            total ^= a[i];
+        }
+        if(n%2 == 1)
+        {
+            cout<<total<<endl;
+        }else
+        {
+            if(total == 0)
+            {
+                cout<<total<<endl;
+            }else
+            {
+                cout<<-1<<endl;
+            }
+        }
+    }
+    return 0;
+}  
